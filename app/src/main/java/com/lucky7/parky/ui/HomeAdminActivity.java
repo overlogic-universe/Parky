1330785342d32df783a1572f89947ddf8c7fa434
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-12 11:07:19 +0700

    style: add home admin page

--- app/src/main/java/com/lucky7/parky/ui/HomeAdminActivity.java
+++ app/src/main/java/com/lucky7/parky/ui/HomeAdminActivity.java
@@ -1,14 +1,16 @@
-package com.lucky7.parky;
+package com.lucky7.parky.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.lucky7.parky.R;
+
+public class HomeAdminActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home_admin);
     }
 }
