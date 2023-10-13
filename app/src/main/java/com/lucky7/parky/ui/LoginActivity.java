03a159f7ca3e438d12ccb2059eb48eb26cc9af1a
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-13 16:06:07 +0700

    feat: make login button to move to admin home page

--- app/src/main/java/com/lucky7/parky/ui/LoginActivity.java
+++ app/src/main/java/com/lucky7/parky/ui/LoginActivity.java
@@ -2,15 +2,28 @@ package com.lucky7.parky.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.lucky7.parky.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+
+    private void initView(){
+        btnLogin = findViewById(R.id.btn_login);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(this, HomeAdminActivity.class));
+        });
     }
 }
