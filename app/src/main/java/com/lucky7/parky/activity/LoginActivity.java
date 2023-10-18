fafc1dd7cb3cbcbd8957cf01069758b70ba74b91
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-18 22:10:50 +0700

    feat: add logic for logging to user page & admin page

--- app/src/main/java/com/lucky7/parky/activity/LoginActivity.java
+++ app/src/main/java/com/lucky7/parky/activity/LoginActivity.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.lucky7.parky.R;
 
+import java.util.Locale;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnLogin;
+    private EditText edEmail, edPass;
 
     private void initView(){
         btnLogin = findViewById(R.id.btn_login);
+        edEmail = findViewById(R.id.ed_email);
+        edPass = findViewById(R.id.ed_pass);
     }
 
     @Override
@@ -24,12 +30,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         initView();
 
         btnLogin.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_login){
-            startActivity(new Intent(this, UserHomeActivity.class));
+            String email = edEmail.getText().toString();
+            String pass = edPass.getText().toString();
+            if(email.toLowerCase(Locale.ROOT).equals("admin@gmail.com") && pass.toLowerCase(Locale.ROOT).equals("admin123")){
+                startActivity(new Intent(this, AdminHomeActivity.class));
+            } else if (email.toLowerCase(Locale.ROOT).equals("user@gmail.com") && pass.toLowerCase(Locale.ROOT).equals("user123")) {
+                startActivity(new Intent(this, UserHomeActivity.class));
+            }
         }
     }
 }
