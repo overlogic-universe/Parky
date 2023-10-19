e941ca881d13280f4cea8facb39510c7f820163d
Sulthon Kaffaah Al Farizzi <l200220279@student.ums.ac.idd>
2023-10-19 19:10:53 +0700

    add Class UserAuth

---
+++ app/src/main/java/com/lucky7/parky/model/UserAuth.java
@@ -0,0 +1,19 @@
+package com.lucky7.parky.model;
+
+
+public class UserAuth extends Auth{
+    String parkStatus;
+    String nim;
+    String plat;
+    String barcodeId;
+    
+public void dologin(){
+
+    }
+
+public void changePass(){
+
+    }
+
+
+}
