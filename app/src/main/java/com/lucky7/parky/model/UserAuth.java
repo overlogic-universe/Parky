098619f18b83a607299c164e14aca72889b5d590
Sulthon Kaffaah Al Farizzi <L200220279@student.ums.ac.id>
2023-10-19 19:28:07 +0700

    New Update

--- app/src/main/java/com/lucky7/parky/model/UserAuth.java
+++ app/src/main/java/com/lucky7/parky/model/UserAuth.java
@@ -1,12 +1,12 @@
 package com.lucky7.parky.model;
 
-
 public class UserAuth extends Auth{
     String parkStatus;
     String nim;
     String plat;
     String barcodeId;
-    
+
+@Override    
 public void dologin(){
 
     }
