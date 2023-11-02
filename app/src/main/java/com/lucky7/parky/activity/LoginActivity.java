0dd174fc4c18d493682cf32e5912d03d0ed80649
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-02 16:59:07 +0700

    feat: add splash screen

--- app/src/main/java/com/lucky7/parky/activity/LoginActivity.java
+++ app/src/main/java/com/lucky7/parky/activity/LoginActivity.java
@@ -25,6 +25,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
         initView();
