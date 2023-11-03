2ea79dd504fa628e6043b961c45d792a8d0491e1
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-03 14:59:53 +0700

    add change password form

---
+++ app/src/main/java/com/lucky7/parky/ChangePasswordFragment.java
@@ -0,0 +1,27 @@
+package com.lucky7.parky;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChangePasswordFragment extends Fragment {
+    public ChangePasswordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        return view;
+    }
+}
