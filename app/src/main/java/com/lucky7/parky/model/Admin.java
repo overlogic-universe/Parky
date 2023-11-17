e84bc13749cc05f2f9c7d3bf460bc85e6ec88c91
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-17 20:56:19 +0700

    overrides the methods from class Authentication to class Admin and User

--- app/src/main/java/com/lucky7/parky/model/Admin.java
+++ app/src/main/java/com/lucky7/parky/model/Admin.java
@@ -2,6 +2,7 @@ package com.lucky7.parky.model;
 
 public class Admin extends Authentication {
 
+
     public Admin(String name,String email ,String password){
         super(name, email, password);
 
@@ -12,5 +13,29 @@ public class Admin extends Authentication {
     }
 
     public void deleteUser(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- app/src/main/java/com/lucky7/parky/model/User.java
+++ app/src/main/java/com/lucky7/parky/model/User.java
@@ -10,13 +10,70 @@ public class User extends  Authentication {
         super(name, email, password);
     }
 
-public void dologin(){
+    public void changePass(){
 
     }
 
-public void changePass(){
+    public void dologin(){
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getParkStatus() {
+        return parkStatus;
+    }
+
+    public void setParkStatus(String parkStatus) {
+        this.parkStatus = parkStatus;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getPlat() {
+        return plat;
+    }
+
+    public void setPlat(String plat) {
+        this.plat = plat;
+    }
+
+    public String getBarcodeId() {
+        return barcodeId;
+    }
+
+    public void setBarcodeId(String barcodeId) {
+        this.barcodeId = barcodeId;
+    }
+
 
 
 }
