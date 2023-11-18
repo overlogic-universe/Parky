9c13db7103dfdc82bc00fe568d847e73fc7340ba
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-18 23:40:39 +0700

    success to add user

--- app/src/main/java/com/lucky7/parky/activity/AddUserActivity.java
+++ app/src/main/java/com/lucky7/parky/activity/AddUserActivity.java
@@ -1,14 +1,20 @@
 package com.lucky7.parky.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.lucky7.parky.R;
@@ -43,13 +49,10 @@ public class AddUserActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
-
         if (v.getId() == R.id.iv_back_from_add_user) {
             getOnBackPressedDispatcher().onBackPressed();
         } else if (v.getId() == R.id.btn_add_user) {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference reference = database.getReference("users");
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
             String username =  edUsername.getText().toString();
             String NIM =  edNIM.getText().toString();
@@ -57,10 +60,21 @@ public class AddUserActivity extends AppCompatActivity implements View.OnClickLi
             String email =  edEmail.getText().toString();
             String password =  edPassword.getText().toString();
 
-            User userClass = new User(username,NIM, plate, email, password );
-            reference.child(username).setValue(userClass);
-            Intent intent = new Intent(AddUserActivity.this, AdminHomeActivity.class);
-            startActivity(intent);
+            User user = new User(username,NIM, plate, email, password );
+            reference.child("users").child(NIM).setValue(user)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(AddUserActivity.this, AdminHomeActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(AddUserActivity.this, "Berhasil menambahkan user.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(AddUserActivity.this, "Gagal menambahkan user.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        Log.e("add user error", "Gagal menambahkan user: " + e.getMessage());
+                        Toast.makeText(AddUserActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
         }
     }
 }
