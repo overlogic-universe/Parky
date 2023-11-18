2abedb9ec15d079f0fc05f82a3f48bea9be48942
mfl4 <mhdfarhanl6267@gmail.com>
2023-11-18 14:29:03 +0700

    make code more clean

--- app/src/main/java/com/lucky7/parky/fragment/ChangePasswordFragment.java
+++ app/src/main/java/com/lucky7/parky/fragment/ChangePasswordFragment.java
@@ -21,8 +21,7 @@ public class ChangePasswordFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_change_password, container, false);
         return view;
     }
