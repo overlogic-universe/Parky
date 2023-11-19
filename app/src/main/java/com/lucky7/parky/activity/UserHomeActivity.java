0c96d0ad8d0eb8ed8b87736d70b452666256f228
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-19 22:00:34 +0700

    feat: genereate student ID to QR Code

--- app/src/main/java/com/lucky7/parky/activity/UserHomeActivity.java
+++ app/src/main/java/com/lucky7/parky/activity/UserHomeActivity.java
@@ -1,13 +1,22 @@
 package com.lucky7.parky.activity;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.lucky7.parky.R;
 import com.lucky7.parky.model.User;
+import com.lucky7.parky.util.BarcodeEncoder;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,11 +25,18 @@ import java.util.Locale;
 public class UserHomeActivity extends AppCompatActivity {
     public static final String EXTRA_USER = "extra_user";
 
+    private User user;
+
+
     private TextView tvUsername;
     private TextView tvDate;
     private TextView tvStudentId;
     private TextView tvPlate;
     private TextView tvStatus;
+
+    private ImageView ivBarcode;
+
+
 
 
     @Override
@@ -33,12 +49,12 @@ public class UserHomeActivity extends AppCompatActivity {
         tvStudentId = findViewById(R.id.tv_the_nim);
         tvPlate = findViewById(R.id.tv_the_plat);
         tvStatus = findViewById(R.id.tv_the_status);
+        ivBarcode = findViewById(R.id.iv_barcode);
 
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
         String currentDate = dateFormat.format(calendar.getTime());
 
-        User user;
         if (android.os.Build.VERSION.SDK_INT >= 33) {
             user = getIntent().getParcelableExtra(EXTRA_USER, User.class);
         } else {
@@ -53,5 +69,22 @@ public class UserHomeActivity extends AppCompatActivity {
             tvStatus.setText(user.getParkStatus());
         }
 
+        convertToBarcode();
+    }
+
+    private void convertToBarcode(){
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(user.getStudentId(), BarcodeFormat.QR_CODE, 250, 250);
+
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            barcodeEncoder.setBackgroundColor(Color.TRANSPARENT);
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+            ivBarcode.setImageBitmap(bitmap);
+        }catch (WriterException e){
+            throw new RuntimeException(e);
+        }
     }
 }
